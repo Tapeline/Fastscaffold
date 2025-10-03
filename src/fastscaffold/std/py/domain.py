7f339325f459abb2c80d63b4c77f1e55e3bfc582
Tapeline <mail@tapeline.dev>
2025-10-03 14:46:39 +0500

    Refactor domain def

--- src/fastscaffold/std/py/domain.py
+++ src/fastscaffold/std/py/domain.py
@@ -1,5 +1,6 @@
 import textwrap
 from dataclasses import dataclass
+from typing import Any
 
 import camelsnake
 
@@ -8,6 +9,8 @@ from fastscaffold.core.context import ScaffoldRunContext
 from fastscaffold.std.configs import ArchitectureConfig, WebProjectConfig
 from fastscaffold.std.gen import src_in
 from fastscaffold.std.helpers import with_src
+
+from src.fastscaffold.std.gen import SimpleTemplateRender
 
 
 @dataclass
@@ -22,12 +25,7 @@ class EntityStore:
     entities: dict[str, DomainEntity]
 
 
-class EntityGen(ScaffoldComponent):
-    requires_context = [
-        WebProjectConfig,
-        ArchitectureConfig
-    ]
-
+class EntityGen(SimpleTemplateRender):
     def __init__(
         self,
         *,
@@ -37,40 +35,27 @@ class EntityGen(ScaffoldComponent):
         make_dataclass: bool = True,
         append: str = "",
         add_imports: list[str] | None = None,
-        filename: str | None = None
     ) -> None:
         self.name = name
         self.fields = fields
         self.make_dataclass = make_dataclass
         self.append = textwrap.dedent(append)
         self.add_imports = add_imports or []
-        self.filename = filename or camelsnake.camel_to_snake(name)
+        self.filename = camelsnake.camel_to_snake(name)
         self.with_id = with_id
+        self.location = ["domain", f"{self.filename}.py"]
 
-    def build(self, ctx: ScaffoldRunContext) -> None:
-        file = ctx.files[src_in("domain", self.filename + ".py")(ctx)]
-        imports = self.add_imports
-        lines = []
-        if self.with_id:
-            imports.append("from typing import NewType")
-            imports.append("from uuid import UUID")
-            lines.append(f'{self.name}Id = NewType("{self.name}Id", UUID)')
-            lines.append("")
-            lines.append("")
-            self.fields = {"id": f"{self.name}Id", **self.fields}
-        if self.make_dataclass:
-            lines.append("@dataclass")
-            imports.append(
-                "from dataclasses import dataclass"
-            )
-        lines.append(f"class {self.name}:")
-        for field_name, field_type in self.fields.items():
-            lines.append(f"    {field_name}: {field_type}")
-        if self.append:
-            lines.extend(
-                textwrap.indent(self.append, "    ").splitlines()
-            )
-        file.lines = [*imports, "", *file.lines, "", *lines, ""]
+    def get_jinja_vars(self, ctx: ScaffoldRunContext) -> dict[str, Any]:
+        return super().get_jinja_vars(ctx) | dict(
+            name=self.name,
+            fields=self.fields,
+            with_id=self.with_id,
+            make_dataclass=self.make_dataclass,
+            append=self.append,
+            add_imports=self.add_imports,
+        )
+
+    def after_build(self, ctx: ScaffoldRunContext) -> None:
         if EntityStore not in ctx:
             ctx += EntityStore({})
         import_line = (
